3cfa98ba091c93fcbb394d28bab3488521c54d6a
Arnar Kari <arnarkari93@gmail.com>
2015-08-18 17:15:33 +0000

    initialize server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+'use-scrict';
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+// var config     = require('./server/config.js');
+// var db         = require('./server/db.js');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// app.set('superSecret', config.secret);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname, 'public/index.html');
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
