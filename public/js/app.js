a09010fc5221d1dd5d3c5f8327210e66dfc89ea9
Arnar Kari <arnarkari93@gmail.com>
2015-08-26 09:56:53 +0000

    added view for about us

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,20 @@ angular.module('ReykjavikHarbour', ['ngRoute']);
 angular.module('ReykjavikHarbour').config(['$routeProvider', function ($routeProvider) {
     $routeProvider
     .when('/home', { templateUrl: '/Views/home.html', controller: 'HomeController' })
+    .when('/AboutUs', { templateUrl: '/Views/about-us.html', controller: 'AboutController' })
     .otherwise({
-		redirectTo: '/'
+		redirectTo: '/home'
 	});
 }]);
 
 // Controllers
 angular.module('ReykjavikHarbour').controller('HomeController', ['$scope', function ($scope) {
+    $scope.Title = "Reykjavik Harbour";
+
+    $scope.array = [1,2,3,4,5];
+}]);
+
+angular.module('ReykjavikHarbour').controller('AboutController', ['$scope', function ($scope) {
     $scope.Title = "Reykjavik Harbour";
 
     $scope.array = [1,2,3,4,5];
